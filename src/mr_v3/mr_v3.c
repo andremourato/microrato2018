c3561b8408ce7df499cc56f4db3330eb3bffe8d5
andremourato <andremourato@ua.pt>
2018-05-01 22:26:39 +0100

    Implementação do P de PID. Ainda tem problemas com biforcações

--- src/mr_v3/mr_v3.c
+++ src/mr_v3/mr_v3.c
@@ -4,21 +4,21 @@
 double getRealSpeed();
 void stopRobot();
 void resetAllVariables();
-void adjustToTheLeft();
-void adjustToTheRight();
-void goForward();
+void adjust(int error);
 void turnRight();
 void turnLeft();
 void invertDirection();
 //**************************** Variáveis do sistema ****************************
 volatile int millis = 0;
 //Constantes
+//Proporionalidade
+double prevError;
+double Kp = 3.5; //Contante de proporcionalidade
 double ROBOT_DIAMETER = 0.120; //Em m
 double ROBOT_RADIUS;
 double MAX_SPEED =  0.15; //Em m/s
 // Velocidades
-int speed = 45; //Percentagem da velocidade máxima do motor. Velocidade em linha reta
-int adjust = 8; //Velocidade de ajuste. Vai ser somado a uma roda e subtraido à outra
+int speed = 50; //Percentagem da velocidade máxima do motor. Velocidade em linha reta
 int turningSpeed = 40; //Velocidade a virar
 volatile int sensor;
 //Delays
@@ -34,23 +34,36 @@ int detectedLineAhead(){return sensor == 0b00100 || sensor == 0b01110 || sensor 
 							   sensor == 0b01000 || sensor == 0b00110 || sensor == 0b00010;}
 void stopRobot(){ setVel2(0,0); }
 void resetAllVariables(){ stopRobot(); }
-void adjustToTheLeft(){ setVel2(speed-adjust,speed+adjust); }
-void adjustToTheRight(){ setVel2(speed+adjust,speed-adjust); }
-void goForward(){ setVel2(speed,speed); }
+void adjust(int error){  //Error pode ter os valores:-3, -2, -1, 0, 1, 2, 3
+	if(error == 3) error = prevError > 0 ? -3 : 3;
+	int leftSpeed = (int)(speed+Kp*(double)error);
+	int rightSpeed = (int)(speed-Kp*(double)error);
+	setVel2(leftSpeed,rightSpeed);
+	//printf("Error=%d | %d | %d\n",error,leftSpeed,rightSpeed);
+	error = prevError;
+}
 void turnRight(){
+	led(2,1);
 	setVel2(turningSpeed,-turningSpeed);
-	delay(TIME_TO_ROTATE_90);
+	//delay(TIME_TO_ROTATE_90);
+	millis = 0;
 	while(!detectedLineAhead()){ sensor = readLineSensors(0); }
+	printf("%d\n",millis);
+	led(2,0);
 }
 void turnLeft(){
+	led(3,1);
 	setVel2(-turningSpeed,turningSpeed);
-	delay(TIME_TO_ROTATE_90);
+	//delay(TIME_TO_ROTATE_90);
 	while(!detectedLineAhead()){ sensor = readLineSensors(0); }
+	led(3,0);
 }
 void invertDirection(){
+	led(4,1);
 	setVel2(-turningSpeed,turningSpeed); //Vira à esquerda até encontrar a linha de novo
-	delay(TIME_TO_ROTATE_90);
+	//delay(TIME_TO_ROTATE_90);
 	while(!detectedLineAhead()){ sensor = readLineSensors(0); }
+	led(4,0);
 }
 
 //**************************** Algoritmos para percorrer o labirinto ****************************
@@ -66,7 +79,6 @@ void findBestPath(){
 					break;
 				//Caso detete biforcação ou meta
 				case 0b11111:
-					
 				//Deteta curva à direita
 				case 0b00001:
 				case 0b00011:
@@ -74,27 +86,29 @@ void findBestPath(){
 				case 0b01111:
 					turnRight();
 					break;
-				case 0b00100:
-				case 0b01110: //Sensores da frente ativos
-					goForward();
-					break;
-				case 0b01100:
-				case 0b01000: //Está a fugir para a direita
-					adjustToTheLeft();
-					break;
-				case 0b00110:
-				case 0b00010: //Está a fugir para a esquerda
-					adjustToTheRight();
-					break;
-				//Deteta curva à esquerda
-				//Neste algoritmo o robot vai ignorar
-				//as curvas à esquerda
-				case 0b10000:
 				case 0b11000:
-				case 0b11100:
-				case 0b11110:
-					//turnLeft();
+				case 0b01000: //Está a fugir para a direita
+					adjust(-2);
 					break;
+				case 0b11100:
+				case 0b01100:
+					adjust(-1);
+					break;
+				case 0b10100:
+				case 0b11110:
+				case 0b00100:
+					adjust(0);
+					break;
+				case 0b10110:
+				case 0b00110:
+					adjust(1);
+					break;
+				case 0b10010:
+				case 0b00010: //Está a fugir para a esquerda
+					adjust(2);
+					break;
+				case 0b01110: //Pode ter que compensar para a esquerda ou direita
+					adjust(3);
 		}
 	}
 }
@@ -114,7 +128,7 @@ int main(void)
 {
 
 	configureRobot(); //Configura o robot
-	//waitingStart();
+	waitingStart();
 	return 0;
 }
 
@@ -146,7 +160,7 @@ void configureRobot(){
 	configureTimer();
 	// Inicializacao da pic
 	initPIC32();
-	closedLoopControl(true); //Ativa a estabilização por PID
+	closedLoopControl(false); //Ativa a estabilização por PID
 	stopRobot();
 }
 
@@ -164,7 +178,7 @@ void configureTimer(){
 }
 
 void calculateVariables(){
-	TIME_TO_ROTATE_90 = 450; //Em ms. Por tentativa erro. Calcular, mais tarde
+	TIME_TO_ROTATE_90 = 300; //Em ms. Por tentativa erro. Calcular, mais tarde
 	ROBOT_RADIUS = ROBOT_DIAMETER/2.0;
 	TIME_TO_CENTER = 1000.0 * (ROBOT_RADIUS - 0.01) / getRealSpeed();
 }
